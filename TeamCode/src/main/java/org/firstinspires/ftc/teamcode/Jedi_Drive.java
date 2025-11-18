945b9de65c5c1e6332704ff8bee6ffc5c31e62a9
CHSRobotics <chsrobotics@gibdist.net>
2025-11-18 14:16:25 -0500

    Shooter Sequence ADD

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jedi_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jedi_Drive.java
@@ -13,6 +13,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import java.util.function.Supplier;
 
@@ -39,6 +40,7 @@ public class Jedi_Drive extends OpMode {
     private DcMotorSimple intake = null;
     private DcMotorSimple shooter = null;
     private DcMotorSimple conveyor = null;
+    private DcMotorSimple prelaunch = null;
 
     // Driving states
     private boolean driveMode = true;
@@ -47,7 +49,10 @@ public class Jedi_Drive extends OpMode {
 
     // Attachment states
     private boolean odometryUp = true;
-    private boolean shooterOn = false;
+    
+    // Launch sequence states
+    private ElapsedTime launchTimer;
+    private boolean launchSequenceActive = false;
 
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -74,8 +79,8 @@ public class Jedi_Drive extends OpMode {
         headLight =hardwareMap.get(Servo.class,"Headlight");
         rgbLight = hardwareMap.get(Servo.class,"RGBLight");
         encoderLift.setPosition(.65);
-        headLight.setPosition(.3);
-        rgbLight.setPosition(.475);
+        headLight.setPosition(0.35);
+        rgbLight.setPosition(0.47);
 
 
         //-------------------
@@ -84,8 +89,10 @@ public class Jedi_Drive extends OpMode {
         intake = hardwareMap.get(DcMotorSimple.class,"Intake");
         shooter = hardwareMap.get(DcMotorSimple.class,"Shooter");
         conveyor = hardwareMap.get(DcMotorSimple.class,"Conveyor");
+        prelaunch = hardwareMap.get(DcMotorSimple.class,"Prelaunch");
 
-
+        // Init launch timer
+        launchTimer = new ElapsedTime();
     }
 
 
@@ -130,7 +137,7 @@ public class Jedi_Drive extends OpMode {
             else follower.setTeleOpDrive(
                     -gamepad1.left_stick_y * slowModeMultiplier,
                     -gamepad1.left_stick_x * slowModeMultiplier,
-                    -gamepad1.right_stick_x * (slowModeMultiplier / 2),
+                    -gamepad1.right_stick_x * (slowModeMultiplier / 1.5),
                     driveMode // Robot Centric
             );
         }
@@ -150,7 +157,7 @@ public class Jedi_Drive extends OpMode {
         */
 
         //Slow Mode
-        slowMode = gamepad1.right_bumper;
+        slowMode = gamepad1.left_bumper;
 
         //Optional way to change slow mode strength
         if (gamepad1.xWasPressed()) {
@@ -182,31 +189,85 @@ public class Jedi_Drive extends OpMode {
         encoderLift.setPosition(odometryUp ? 0.65 : 0);
 
 
-    //------------------
-    //--MOTOR CONTROLS--
-    //------------------
-
-        // If DP-Left button is pressed on GP-1 toggle the shooterOn variable
-        shooterOn = gamepad2.right_trigger > 0.75;
-
-        // Set shooter power based on the toggled state
-        shooter.setPower(shooterOn ? -1.0 : 0.0);
-
-        // Control Intake and Conveyor
-        if (gamepad2.a) {
-            // Run forward when 'a' is pressed
-            intake.setPower(1.0);
-            conveyor.setPower(-1.0);
-        } else if (gamepad2.b) {
-            // Run in reverse when 'b' is pressed
-            intake.setPower(-1.0);
-            conveyor.setPower(1.0);
+    //-------------------
+    //-- RGB INDICATOR --
+    //-------------------
+    if (launchSequenceActive) {
+        // Create a flash effect by casting the timer result to a whole number before the modulo
+        if (((long) (launchTimer.milliseconds() / 250)) % 2 == 0) {
+            rgbLight.setPosition(0.28); // Red
+            headLight.setPosition(0.3);
         } else {
-            // Turn off motors if neither 'a' nor 'b' is pressed
-            intake.setPower(0.0);
-            conveyor.setPower(0.0);
+            rgbLight.setPosition(0.0); // Green (or off)
+            headLight.setPosition(0.0);
+        }
+    } else {
+        // When not launching, keep the light solid green
+        rgbLight.setPosition(0.47);
+        headLight.setPosition(0.3);
+    }
+
+
+    //---------------------------
+    //--LAUNCHER MOTOR CONTROLS--
+    //---------------------------
+
+        // --- AUTOMATED LAUNCH SEQUENCE ---
+        boolean launchTriggerPressed = gamepad2.right_trigger > 0.75;
+
+        // Default motor powers
+        double shooterPower = 0.0;
+        double intakePower = 0.0;
+        double conveyorPower = 0.0;
+        double prelaunchPower = 0.0;
+
+        // Check if the trigger is pressed to start or continue the sequence
+        if (launchTriggerPressed) {
+            // If this is the first moment the trigger is pressed, reset the timer
+            if (!launchSequenceActive) {
+                launchSequenceActive = true;
+                launchTimer.reset();
+            }
+
+            // Shooter spins up immediately
+            shooterPower = -1.0;
+
+            // After 2 seconds, also run the feeder motors
+            if (launchTimer.seconds() > 1.75) {
+                intakePower = 1.0;
+                conveyorPower = -1.0;
+                prelaunchPower = 1.0;
+            }
+        } else {
+            // If the trigger is not pressed, the sequence is not active
+            launchSequenceActive = false;
         }
 
+        // --- MANUAL REVERSE (for clearing jams) ---
+        // This overrides the launch sequence if 'b' is pressed.
+        if (gamepad2.y) {
+            intakePower = -1.0;
+            conveyorPower = 1.0;
+            prelaunchPower = 0.0; // Make sure prelaunch doesn't run in reverse
+            shooterPower = 0.0;   // Stop shooter during reverse
+        }
+
+        // --- SET FINAL MOTOR POWERS ---
+        shooter.setPower(shooterPower);
+        intake.setPower(intakePower);
+        conveyor.setPower(conveyorPower);
+        prelaunch.setPower(prelaunchPower);
+
+
+    //--------------------------
+    //--GENERAL MOTOR CONTROLS--
+    //--------------------------
+
+        if (gamepad2.a || gamepad1.right_bumper) {
+            intake.setPower(1.0);
+            conveyor.setPower(-1.0);
+            prelaunch.setPower(-0.30);
+        }
 
 
     //------------------
@@ -216,6 +277,7 @@ public class Jedi_Drive extends OpMode {
         telemetry.addData("Slow Mode Multiplier", slowModeMultiplier);
         telemetry.addData("Slow Mode", slowMode);
         telemetry.addData("Current Servo Position", encoderLift.getPosition());
+        telemetry.addData("Launch Sequence Active", launchSequenceActive);
 
         if (odometryUp) {
             telemetry.addLine("Odometry UP");
